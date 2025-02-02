ccb59f63abc6bf6aad279f033aa6e25dbfcb606a
Edison Pinanjota <edisonpinanjota95@gmail.com>
2025-02-01 19:18:12 -0500

    ✨ Ticket-1: Agregar archivo Readme

--- obj/Debug/net9.0/WebApiExample3.AssemblyInfo.cs
+++ obj/Debug/net9.0/WebApiExample3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApiExample3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07282493695b3f62b94636bb20df24038eb9f6e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApiExample3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApiExample3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
